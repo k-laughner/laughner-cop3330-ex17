27a2046eee2e83bb5cc7d05c84c12d5d9974a644
k-laughner <74153467+k-laughner@users.noreply.github.com>
2021-09-13 16:40:58 -0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,44 +3,55 @@
  *  Copyright 2021 Kaylee Laughner
  */
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Main {
+    static Scanner console = new Scanner(System.in);
+    static public char restart = 'n';
+
     public static void main(String[] args) {
-        System.out.println("Enter a 1 if you are male or a 2 if you are female:");
-        Scanner input = new Scanner(System.in);
-        int sex = input.nextInt();
+      do {
+        try{
+          System.out.println("Enter a 1 if you are male or a 2 if you are female:");
+          int sex = console.nextInt();
 
-        System.out.println("How many ounces of alcohol did you have?");
-        Scanner input2 = new Scanner(System.in);
-        float oz = input2.nextFloat();
+          System.out.println("How many ounces of alcohol did you have?");
+          float oz = console.nextFloat();
 
-        System.out.println("What is your weight, in pounds?");
-        Scanner input3 = new Scanner(System.in);
-        int lb = input3.nextInt();
+          System.out.println("What is your weight, in pounds?");
+          int lb = console.nextInt();
 
-        System.out.println("How many hours has it been since your last drink?");
-        Scanner input4 = new Scanner(System.in);
-        int hours = input4.nextInt();
+          System.out.println("How many hours has it been since your last drink?");
+          int hours = console.nextInt();
 
-        double r = 0;
+          double r = 0;
 
-        if(sex==1){
-            r = 0.73;
+          if(sex==1){
+              r = 0.73;
+          }
+          else if (sex==2){
+              r = 0.66;
+          }
+          else{
+              System.out.println("Please enter 1 or 2 to indicate your sex.");
+          }
+          double bac = (oz*(5.14/lb)*r) - (0.015*hours);
+          System.out.printf("Your BAC is %.5f.\n", bac);
+
+          if(bac<0.08){
+              System.out.println("It is legal for you to drive.");
+          }
+          else{
+              System.out.println("It is not legal for you to drive.");
+          }
+          break;
         }
-        else if (sex==2){
-            r = 0.66;
+        catch (InputMismatchException fe){
+          System.out.println("You entered the incorrect data type. Please try again.");
+          restart='y';
+          console.next();
         }
-        else{
-            System.out.println("Please enter 1 or 2 to indicate your sex.");
-        }
-        double bac = (oz*(5.14/lb)*r) - (0.015*hours);
-        System.out.println(bac);
-
-        if(bac<0.08){
-            System.out.println("It is legal for you to drive.");
-        }
-        else{
-            System.out.println("It is not legal for you to drive.");
-        }
-    }
+      }
+      while (restart == 'y');
+  }
 }
